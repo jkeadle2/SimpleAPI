6788584f6bb837f26fc0717ef5a062c379e5b408
James Keadle <jkeadle2@gmail.com>
2022-02-04 09:25:35 -0500

    Initial Commit

---
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2"};
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Freezing";
+        }
+    }
+}
---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.TEST
+{
+    public class UnitTest1
+    {
+
+        WeatherForecastController controller = new WeatherForecastController();
+        
+        [Fact]
+        public void Test1()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Freezing", returnValue.Value);
+
+        }
+    }
+}
