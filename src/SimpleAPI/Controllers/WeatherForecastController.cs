257980543246bf7aabbd71abcdc5cad97402718b
James Keadle <jkeadle2@gmail.com>
2022-02-09 16:27:42 -0500

    changed return values

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -15,7 +15,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2"};
+            return new string[] { "demo", "test"};
         }
 
         // GET api/values/5
